d0813ef89113f0f4bf1c473c7e6bf8183a28f3e4
Thiago <dev.tbsantos@gmail.com>
2024-05-02 17:58:41 -0300

    Criação dos elementos visuais e da sua responsividade - Seção de apresentação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from '../styles/HomeNoAuth.module.scss'
 import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
+import PresentationSection from "@/components/homeNoAuth/presentationSection";
 
 
 const HomeNoAuth = () => {
@@ -13,7 +14,10 @@ const HomeNoAuth = () => {
         <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de forma fácil!" />
       </Head>
       <main>
+        <div className={styles.sectionBackground}>
         <HeaderNoAuth />
+        <PresentationSection />
+        </div>
       </main>
     </>
   )
