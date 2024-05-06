7771837e599a2cf99b2de3787bf5cd63f7a33b7b
Thiago <dev.tbsantos@gmail.com>
2024-05-06 16:58:58 -0300

    Criação dos elementos do header logado, do seu estilo visual e sua responsividade

---
+++ src/pages/home.tsx
@@ -0,0 +1,17 @@
+import HeaderAuth from "@/components/common/headerAuth"
+import Head from "next/head"
+import { Container } from 'reactstrap'
+
+const HomeAuth = function () {
+    return <>
+        <Head>
+        <title>Onebitflix - Home</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        </Head>
+        <main>
+            <HeaderAuth />
+        </main>
+    </>
+}
+
+export default HomeAuth
