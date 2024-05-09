ef8b8045de0a16ea0e4e17da27e4eb578cb28903
Thiago <dev.tbsantos@gmail.com>
2024-05-09 11:27:30 -0300

    Criação dos elementos de Search, do seu estilo visual e da sua responsividade completa

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -5,21 +5,23 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import courseService, { CourseType } from "@/services/courseService";
+import { Container } from "reactstrap";
+import SearchCard from "@/components/searchCard";
+import Footer from "@/components/common/footer";
 
 const Search = function () {
-    const router = useRouter()
-    const searchName: any = router.query.name
-    const [searchResult, setSearchResult] = useState<CourseType[]>([])
+  const router = useRouter();
+  const searchName: any = router.query.name;
+  const [searchResult, setSearchResult] = useState<CourseType[]>([]);
 
-    const searchCourses = async function () {
-            const res = await courseService.getSearch(searchName)
-            setSearchResult(res.data.courses)
-    };
+  const searchCourses = async function () {
+    const res = await courseService.getSearch(searchName);
+    setSearchResult(res.data.courses);
+  };
 
-    useEffect(() => {
-        searchCourses()
-    }, [searchName])
-
+  useEffect(() => {
+    searchCourses();
+  }, [searchName]);
 
   return (
     <>
@@ -27,16 +29,29 @@ const Search = function () {
         <title>Onebitflix - {searchName}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
-        <HeaderAuth />
-        {searchResult?.map((course) => (
-            <div key={course.id}>
-                <p>{course.name}</p>
-            </div>
-        ))}
+      <main className={styles.main}>
+        <div className={styles.headFooterBg}>
+          <HeaderAuth />
+        </div>
+
+        {searchResult.length >= 1 ? (
+          <div className={styles.searchResult}>
+            <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+            {searchResult?.map((course) => (
+              <SearchCard key={course.id} course={course} />
+            ))}
+          </Container>
+          </div>
+        ) : (
+          <p className={styles.noSearchResult}>Nenhum resultado encontrado</p>
+        )}
+
+        <div className={styles.headFooterBg}>
+          <Footer />
+        </div>
       </main>
     </>
   );
 };
 
-export default Search
+export default Search;
