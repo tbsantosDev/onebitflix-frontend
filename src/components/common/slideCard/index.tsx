20fbc3ec6d6aa86100cfb8a4978db9ffc2730cbe
Thiago <dev.tbsantos@gmail.com>
2024-05-09 14:12:44 -0300

    Criação do metodo de pegar o objeto de um curso e comunicação com o front

--- src/components/common/slideCard/index.tsx
+++ src/components/common/slideCard/index.tsx
@@ -1,18 +1,29 @@
-import styles from "./styles.module.scss"
-import { CourseType } from "@/services/courseService"
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { CourseType } from "@/services/courseService";
 
 interface props {
-    course: CourseType
+  course: CourseType;
 }
 
-const SlideCard = function ({course}: props) {
-    return <>
+const SlideCard = function ({ course }: props) {
+  return (
+    <>
+    <Link style={{textDecoration: "none"}} href={`/course/${course.id}`}>
     <div className={styles.slide}>
-        <img src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} alt={course.name} className={styles.slideImg}/>
+        <img
+          src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
+          alt={course.name}
+          className={styles.slideImg}
+        />
         <p className={styles.slideTitle}>{course.name}</p>
         <p className={styles.slideDescription}>{course.synopsis}</p>
-    </div>
-    </>
-}
+      </div>
+    </Link>
 
-export default SlideCard
+    </>
+  );
+};
+
+export default SlideCard;
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -86,6 +86,34 @@ const courseService = {
       });
     return res;
   },
+  like: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .post("/likes",{courseId}, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+    return res;
+  },
+  removeLike: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .delete(`/likes/${courseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+    return res;
+  },
   getSearch: async (name: string) => {
     const token = sessionStorage.getItem("onebitflix-token");
 
@@ -100,5 +128,20 @@ const courseService = {
       });
     return res;
   },
+  getEpisodes: async (id: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get(`/courses/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+    return res;
+  },
+
 };
 export default courseService;
